f6c29b07b25e0cce869e3596fa6adcf281967bce
Pawel <paweljozwiak1986@gmail.com>
2019-01-23 15:22:02 +0100

    Passing down a function from the Board

--- src/index.js
+++ src/index.js
@@ -21,7 +21,10 @@ class Board extends React.Component {
     squares: Array(9).fill(null)
   };
   renderSquare(i) {
-    return  <Square value={i}/>;
+    return <Square 
+    value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
