ac4db429ebc032961993fcf238d91695d74546ef
Pawel <paweljozwiak1986@gmail.com>
2019-01-23 15:36:09 +0100

    Square's  props.onCLick

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  state = {
-    value: null
-    };
+  
   render() {
     return (
-      <button className="square" 
-      onClick={()=> this.setState({value:'X'})}>
-        {this.state.value}
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
@@ -20,6 +20,7 @@ class Board extends React.Component {
   state = {
     squares: Array(9).fill(null)
   };
+  
   renderSquare(i) {
     return <Square 
     value={this.state.squares[i]} 
